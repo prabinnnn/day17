13788bb6f70f1fda2a19057b54214d96ff07d199
prabinnnn <prabinbhandari120@gmail.com>
2024-01-07 19:16:44 +0545

    add d

---
+++ date-1/d.js
@@ -0,0 +1,7 @@
+// let mydate = new Date();
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocalString());
+// console.log(typeof mydate);
+let createnewdate = new Date(2023, 0, 23);
+console.log(createnewdate);
